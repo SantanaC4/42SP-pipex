b1c4005d07c8243cf2a470311ace190fb568f5f5
SantanaC4 <eduardo.sanati@gmail.com>
2022-04-29 16:16:21 -0300

    add suport to second command

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: edrodrig <edrodrig@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 18:15:55 by edrodrig          #+#    #+#             */
-/*   Updated: 2022/04/29 15:47:57 by edrodrig         ###   ########.fr       */
+/*   Updated: 2022/04/29 16:13:31 by edrodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,13 +53,14 @@ int main(int argc, char* argv[])
 	char *firstComand;
 	char *secondComand;
 	char *argVec[] = {argv[2],"-c","5","gooogle.com", (char*)0};
-	char *argVec2[] = {argv[2],"-c","5","gooogle.com", (char*)0};
+	char *argVec2[] = {argv[3],"rtt", (char*)0};
 	int fd_out;
 	int fd_in;
 
 	fd_in = open(argv[1], O_RDONLY);
 	fd_out = open(argv[4],O_WRONLY);
 	firstComand = ft_strcat("/usr/bin/",argv[2]);
+	secondComand = ft_strcat("/usr/bin/",argv[3]);
 	printf("%s", firstComand);
 	int fd[2];
 	if (pipe(fd) == -1)
@@ -97,7 +98,9 @@ int main(int argc, char* argv[])
 		close(fd_out);
 		close(fd[0]);
 		close(fd[1]);
-		execlp("grep", "grep", "rtt", NULL);
+
+		if (execve(secondComand, argVec2, NULL) == -1)
+			perror("Could not execute execve");
 	}
 
 	close(fd[0]);
