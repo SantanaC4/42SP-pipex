42bc9ed457120d1fee7cff7c39ec9a1e045776e1
SantanaC4 <eduardo.sanati@gmail.com>
2022-04-29 15:51:26 -0300

    Add support to file in and out

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: edrodrig <edrodrig@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 18:15:55 by edrodrig          #+#    #+#             */
-/*   Updated: 2022/04/29 12:27:30 by edrodrig         ###   ########.fr       */
+/*   Updated: 2022/04/29 15:47:57 by edrodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 
 int ft_strlen(char *str)
@@ -50,9 +51,15 @@ char *ft_strcat(char *path, char *command)
 int main(int argc, char* argv[])
 {
 	char *firstComand;
-	char *argVec[] = {"ping","-c","5","gooogle.com", (char*)0};
+	char *secondComand;
+	char *argVec[] = {argv[2],"-c","5","gooogle.com", (char*)0};
+	char *argVec2[] = {argv[2],"-c","5","gooogle.com", (char*)0};
+	int fd_out;
+	int fd_in;
 
-	firstComand = ft_strcat("/usr/bin/",argv[1]);
+	fd_in = open(argv[1], O_RDONLY);
+	fd_out = open(argv[4],O_WRONLY);
+	firstComand = ft_strcat("/usr/bin/",argv[2]);
 	printf("%s", firstComand);
 	int fd[2];
 	if (pipe(fd) == -1)
@@ -65,13 +72,16 @@ int main(int argc, char* argv[])
 	if (pid1 == 0)
 	{
 		// Child process 1 (ping)
+		dup2(fd_in, STDIN_FILENO);
 		dup2(fd[1], STDOUT_FILENO);
+		close(fd_in);
 		close(fd[0]);
 		close(fd[1]);
 
 		if (execve(firstComand, argVec, NULL) == -1)
 			perror("Could not execute execve");
 	}
+
 
 	int pid2 = fork();
 	if (pid2 < 0)
@@ -83,6 +93,8 @@ int main(int argc, char* argv[])
 	{
 		// Child process 2 (grep)
 		dup2(fd[0], STDIN_FILENO);
+		dup2(fd_out, STDOUT_FILENO);
+		close(fd_out);
 		close(fd[0]);
 		close(fd[1]);
 		execlp("grep", "grep", "rtt", NULL);
